5f53225b3b886047ae3c46844ecf1d8e838ee2d5
MVargasVigo <mauricio.vargas98@gmail.com>
2024-03-19 01:53:19 -0500

    challenge completado

--- assignments.js
+++ assignments.js
@@ -477,3 +477,60 @@ const logBookTheme = function (title) {
     );
   }
 };
+
+/* 17.1 Below is the bookCategories variable that stores a string of categories. Each category is separated with a semicolon, for example, in a string "science;computing", 'science' and 'computing' are separate categories.
+
+Write a function called logBookCategories that takes a string of categories separated with semicolons, and logs each category to the console (as separate strings). */
+
+const logBookCategories = function (string) {
+  const categoryArray = string.split(';');
+  for (let category of categoryArray) {
+    console.log(category);
+  }
+};
+
+/* 17.2 Now, the opposite. Each book from the books array has the keywords property.
+
+Write a function called getKeywordsAsString that takes the books array as an argument, collects keywords from each book, removes duplicates, and then joins them to create a single string where keywords are separated by a semicolon. */
+
+const getKeywordsAsString = function (books) {
+  const arrayOfUniques = [];
+  for (let book of books) {
+    const keywordsArray = book.keywords;
+    for (let i = 1; i <= keywordsArray.length; i++) {
+      if (keywordsArray[i] !== keywordsArray[i - 1]) {
+        arrayOfUniques.push(keywordsArray[i]);
+      }
+    }
+    const keywordStrings = arrayOfUniques.join(';');
+    console.log(keywordStrings);
+  }
+};
+
+getKeywordsAsString(books);
+
+//Check in the console
+
+/* 17.3 Below is the bookChapters array that contains inner arrays. Each inner array consists of a chapter's title, and the number of a page, for example, in ['The Basics', 14], 'The Basics' is the chapter's title, and 14 is the number of a page.
+
+Write a function called logBookChapters that takes an array of arrays (like bookChapters) as an argument, and logs each chapter's name to the console together with the page number. The page number should be separated from the chapter's name with underscores (take a look at the example below).
+
+Use the padEnd method. */
+
+const logBookChapters = function (bookChapters) {
+  for (let chapter of bookChapters) {
+    const chapterName = chapter[0].padEnd(15, '_');
+    const chapterInfo = chapterName + ' ' + chapter[1];
+    console.log(chapterInfo);
+  }
+};
+//Check in the console
+const bookChapters = [
+  ['The Basics', 14],
+  ['Sorting', 254],
+  ['Searching', 372],
+  ['Graphs', 526],
+  ['Strings', 706],
+];
+logBookChapters(bookChapters);
+logBookChapters(bookChapters);
